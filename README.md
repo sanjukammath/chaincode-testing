feab89ba4c3dcd8635597a52a7b07ba840f5c17c
Sanjay S B <sanjukammath@gmail.com>
2019-02-06 12:31:32 +0530

    Update the readme and docker installation files as per testing

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ $ vagrant up
 
 $ vagrant ssh
 
+In the vagrant shell vagrant@vagrant:/vagrant$, run the following commands
+
+$ cd setups/
+$ chmod 777 install-docker.sh
+$ sudo bash ./install-docker.sh
+
+On prompt Do you want to continue? [Y/n] enter y (might need to do it twice)
+
--- setups/install-docker.sh
+++ setups/install-docker.sh
@@ -1,13 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 # Install docker in the virtual machine using this script
 # For more detail visit https://docs.docker.com/install/linux/docker-ce/ubuntu/
-
-# The commands need to be executed with sudo. use sudo ./install-docker.sh
-if [ -z $SUDO_USER ]
-then
-    echo "===== script needs to be executed by super user ===="
-    exit 0
-fi
 
 # SET UP THE REPOSITORY
 
@@ -15,19 +8,12 @@ fi
 apt-get update
 
 # install packages to allow apt to use a repository over HTTPS
-apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 # add docker's official gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-# verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88, 
-# by searching for the last 8 characters of the fingerprint.
-
+# verify that you now have the key with the fingerprint 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88,
 apt-key fingerprint 0EBFCD88
 
 # pub   rsa4096 2017-02-22 [SCEA]
@@ -36,26 +22,13 @@ apt-key fingerprint 0EBFCD88
 # sub   rsa4096 2017-02-22 [S]
 
 # set up the stable repository
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+# INSTALL DOCKER CE
 # update the apt package
 apt-get update
 
-# install latest version of 
+# install latest version of docker
 apt-get install docker-ce docker-ce-cli containerd.io
 
-echo "trying to run the hello world container"
-
 docker run hello-world
-
-# logout of vagrant. Login to check if docker is installed properly
-
-echo "vagrant ssh and check docker --version"
-exit
-
-
-
-
